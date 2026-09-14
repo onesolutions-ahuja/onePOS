f37d411b8cf320ad082e3e43857e07f9f1b0cfed
onesolutions-ahuja <tanha.ahuja2@gmail.com>
2026-09-14 14:48:55 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+    allowedHosts: true,
+  },
+
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: true,
+  },
+});
